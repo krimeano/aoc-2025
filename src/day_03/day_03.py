937e4f2ec91d0c68a1d3d4c4b392757456645a62
Sergii Paliutin <spalutin@flutterint.com>
2025-12-03 14:57:24 +0000

    day 03x2

--- src/day_03/day_03.py
+++ src/day_03/day_03.py
@@ -7,12 +7,15 @@ def solve_1(text_data: str, debug=False) -> int:
 
 
 def solve_2(text_data: str, debug=False) -> int:
-    debug and print()
-    return 0
+    result = 0
+    for line in text_data.split():
+        result += find_largest_joltage([int(x.strip()) for x in line if x], 12, debug)
+    return result
 
 
 def find_largest_joltage_simple(batteries: list[int], debug=True) -> int:
-    debug and print('batteries', batteries, end=' -> ')
+    debug and print()
+    debug and print('batteries', batteries)
     a = 0
     b = 0
     for x in batteries:
@@ -21,8 +24,30 @@ def find_largest_joltage_simple(batteries: list[int], debug=True) -> int:
             b = x
         if b < x:
             b = x
-    debug and print(a, b)
+    debug and print('    ', a, b)
     return a * 10 + b
+
+
+def find_largest_joltage(batteries: list[int], size=12, debug=True) -> int:
+    debug and print()
+    debug and print('batteries', batteries)
+    output = []
+    width = len(batteries)
+    min_ix = -1
+
+    for ix in range(width - size, width):
+        debug and print(ix, batteries[ix])
+        m = batteries[ix]
+        min_jy = ix
+        for jy in range(ix, min_ix, -1):
+            y = batteries[jy]
+            if y >= m:
+                min_jy = jy
+                m = y
+        output.append(m)
+        min_ix = min_jy
+    debug and print('    ', output)
+    return int(''.join([str(x) for x in output]))
 
 
 if __name__ == '__main__':
--- src/day_03/test_day_03.py
+++ src/day_03/test_day_03.py
@@ -1,4 +1,4 @@
-from .day_03 import solve_1, solve_2, find_largest_joltage_simple
+from .day_03 import solve_1, solve_2, find_largest_joltage_simple, find_largest_joltage
 
 test_input = """987654321111111
 811111111111119
@@ -22,9 +22,20 @@ def test_find_largest_joltage():
     assert find_largest_joltage_simple([8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1]) == 92
 
 
+def test_find_largest_joltage():
+    assert find_largest_joltage([9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1], 2) == 98
+    assert find_largest_joltage([8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9], 2) == 89
+    assert find_largest_joltage([2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8], 2) == 78
+    assert find_largest_joltage([8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1], 2) == 92
+    assert find_largest_joltage([9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1], 12) == 987654321111
+    assert find_largest_joltage([8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9], 12) == 811111111119
+    assert find_largest_joltage([2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8], 12) == 434234234278
+    assert find_largest_joltage([8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1], 12) == 888911112111
+
+
 def test_solve_1():
-    assert solve_1(test_input, True) == 357
+    assert solve_1(test_input, False) == 357
 
 
 def test_solve_2():
-    assert solve_2(test_input, True) == 0
+    assert solve_2(test_input, True) == 3121910778619
