6a4d51f4038674f79de4c286a4c785bba696fd7b
Sergii Paliutin <spalutin@flutterint.com>
2025-12-04 15:43:38 +0000

    fish

---
+++ src/day_09/day_09.py
@@ -0,0 +1,21 @@
+def solve_1(text_input: str, debug=False) -> int:
+    debug and print()
+    return 0
+
+
+def solve_2(text_input: str, debug=False) -> int:
+    debug and print()
+    return 0
+
+
+if __name__ == '__main__':
+    print('Day 09')
+
+    with open('../../input/09.txt', 'r') as f:
+        file_contents = f.read()
+
+    print('1:')
+    print(solve_1(file_contents))
+
+    print('2:')
+    print(solve_2(file_contents))
---
+++ src/day_10/day_10.py
@@ -0,0 +1,21 @@
+def solve_1(text_input: str, debug=False) -> int:
+    debug and print()
+    return 0
+
+
+def solve_2(text_input: str, debug=False) -> int:
+    debug and print()
+    return 0
+
+
+if __name__ == '__main__':
+    print('Day 10')
+
+    with open('../../input/10.txt', 'r') as f:
+        file_contents = f.read()
+
+    print('1:')
+    print(solve_1(file_contents))
+
+    print('2:')
+    print(solve_2(file_contents))
