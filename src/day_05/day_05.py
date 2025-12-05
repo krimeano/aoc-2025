99ec38e81bbb9ab10a0bd5e0cba69c5a400aaf27
Sergii Paliutin <spalutin@flutterint.com>
2025-12-05 08:23:31 +0000

    day 05x1

--- src/day_05/day_05.py
+++ src/day_05/day_05.py
@@ -1,6 +1,33 @@
 def solve_1(text_input: str, debug=False) -> int:
     debug and print()
-    return 0
+    lines = text_input.splitlines()
+    mode = 'ranges'
+    ranges: list[tuple[int, int]] = []
+    result = 0
+    for line in lines:
+        if not line:
+            mode = 'ids'
+            continue
+
+        debug and print(mode, line)
+
+        if mode == 'ranges':
+            az = [int(x.strip()) for x in line.split('-') if x]
+            ranges.append((az[0], az[1]))
+            continue
+
+        x = int(line.strip())
+
+        fresh = False
+
+        for (a, z) in ranges:
+            if a <= x <= z:
+                fresh = True
+                break
+
+        result += fresh
+
+    return result
 
 
 def solve_2(text_input: str, debug=False) -> int:
--- src/day_05/test_day_05.py
+++ src/day_05/test_day_05.py
@@ -1,11 +1,21 @@
 from .day_05 import solve_1, solve_2
 
-test_input = """
+test_input = """3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
 """
 
 
 def test_solve_1():
-    assert solve_1(test_input, True) == 0
+    assert solve_1(test_input, True) == 3
 
 
 def test_solve_2():
