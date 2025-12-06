62586f543c3c2e6e109670393b17f2a7afdea4ae
Sergii Paliutin <spalutin@flutterint.com>
2025-12-06 10:13:11 +0000

    day 06x1

--- src/day_06/day_06.py
+++ src/day_06/day_06.py
@@ -1,6 +1,24 @@
 def solve_1(text_input: str, debug=False) -> int:
     debug and print()
-    return 0
+
+    result = 0
+    matrix = [[y.strip() for y in x.split()] for x in text_input.splitlines()]
+    operations = matrix.pop()
+
+    for ix in range(len(operations)):
+        op = operations[ix]
+        value = op == '*' and 1 or 0
+
+        for row in matrix:
+            current = int(row[ix])
+            if op == '*':
+                value *= current
+            if op == '+':
+                value += current
+
+        result += value
+
+    return result
 
 
 def solve_2(text_input: str, debug=False) -> int:
@@ -11,7 +29,7 @@ def solve_2(text_input: str, debug=False) -> int:
 if __name__ == '__main__':
     print('Day 06')
 
-    with open('../../input/05.txt', 'r') as f:
+    with open('../../input/06.txt', 'r') as f:
         file_contents = f.read()
 
     print('1:')
--- src/day_06/test_day_06.py
+++ src/day_06/test_day_06.py
@@ -1,11 +1,14 @@
 from .day_06 import solve_1, solve_2
 
-test_input = """
+test_input = """123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  
 """
 
 
 def test_solve_1():
-    assert solve_1(test_input, True) == 0
+    assert solve_1(test_input, True) == 4277556
 
 
 def test_solve_2():
