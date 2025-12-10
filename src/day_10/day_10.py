48048efbbe885e9b0536f414f373fc95252e0271
Sergii Paliutin <spalutin@flutterint.com>
2025-12-10 11:38:38 +0000

    day 10x1

--- src/day_10/day_10.py
+++ src/day_10/day_10.py
@@ -1,11 +1,82 @@
 def solve_1(text_input: str, debug=False) -> int:
     debug and print()
-    return 0
+    result = 0
+
+    for line in text_input.splitlines():
+        if not line:
+            continue
+        lights, buttons, tail = parse_line(line)
+        result += push_buttons(lights, buttons, debug=debug)
+
+    return result
 
 
 def solve_2(text_input: str, debug=False) -> int:
     debug and print()
     return 0
+
+
+def parse_line(line: str, debug=False) -> tuple[int, list[int], list[int]]:
+    parts = line.split()
+    debug and print(parts)
+    lights = parse_lights(parts.pop(0))
+    tail = parse_tail(parts.pop())
+    buttons = [parse_button(x) for x in parts]
+    debug and print(lights, buttons, tail)
+    return lights, buttons, tail
+
+
+def parse_lights(lights: str) -> int:
+    if lights.startswith('['):
+        lights = lights[1:-1]
+    out = 0
+    for char in lights[::-1]:
+        out = (out << 1) + (char == '#')
+    return out
+
+
+def parse_button(button: str) -> int:
+    if button.startswith('('):
+        button = button[1:-1]
+    out = 0
+    for x in button.split(','):
+        out += 2 ** int(x)
+    return out
+
+
+def parse_tail(tail: str) -> list[int]:
+    if tail.startswith('{'):
+        tail = tail[1:-1]
+    return [int(x) for x in tail.split(',')]
+
+
+def push_buttons(lights: int, buttons: list[int], debug=False) -> int:
+    debug and print(lights, buttons)
+    visited_states = {lights}
+
+    new_states = [lights]
+    pushes = 0
+
+    while new_states:
+        states = new_states
+        new_states = []
+
+        pushes += 1
+
+        while states:
+            state = states.pop()
+
+            for button in buttons:
+                new_state = state ^ button
+
+                if not new_state:
+                    return pushes
+
+                if new_state not in visited_states:
+                    visited_states.add(new_state)
+                    new_states.append(new_state)
+
+    return -1
 
 
 if __name__ == '__main__':
--- src/day_10/test_day_10.py
+++ src/day_10/test_day_10.py
@@ -1,11 +1,36 @@
-from .day_10 import solve_1, solve_2
+from .day_10 import solve_1, solve_2, parse_button, parse_lights
 
-test_input = """
+test_input = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
 """
 
 
+def test_parse_lights():
+    assert parse_lights('[#]') == 1
+    assert parse_lights('[.]') == 0
+    assert parse_lights('[.#]') == 2
+    assert parse_lights('[#.]') == 1
+    assert parse_lights('###') == 7
+    assert parse_lights('.##') == 6
+    assert parse_lights('#.#.#.#') == 85
+
+
+def test_parse_button():
+    assert parse_button('(0)') == 1
+    assert parse_button('(1)') == 2
+    assert parse_button('2') == 4
+    assert parse_button('0,1') == 3
+    assert parse_button('(0,2)') == 5
+    assert parse_button('(0,1,2)') == 7
+    assert parse_button('(1,5,6,7,8)') == 482
+
+
 def test_solve_1():
-    assert solve_1(test_input, True) == 0
+    assert solve_1("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}", True) == 2
+    assert solve_1("[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}", True) == 3
+    assert solve_1("[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}", True) == 2
+    assert solve_1(test_input, True) == 7
 
 
 def test_solve_2():
