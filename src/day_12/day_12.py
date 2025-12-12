6a3c79245570fee583c37c89fbdeadb8b9f60bd2
Sergii Paliutin <spalutin@flutterint.com>
2025-12-12 14:06:17 +0000

    day 12x

--- src/day_12/day_12.py
+++ src/day_12/day_12.py
@@ -1,11 +1,44 @@
 def solve_1(text_input: str, debug=False) -> int:
     debug and print()
+    presents, containers = parse_input(text_input.splitlines())
+    print(presents)
+    print(containers)
     return 0
 
 
 def solve_2(text_input: str, debug=False) -> int:
     debug and print()
     return 0
+
+
+def parse_input(lines: list[str]) -> tuple[list, list]:
+    mode = 'present'
+    presents = []
+    containers = []
+    rows = []
+    for line in lines:
+        line = line.strip()
+        if not line:
+            if mode == 'present':
+                mode = ''
+                if rows:
+                    present = tuple([tuple([x == '#' and 1 or 0 for x in row]) for row in rows])
+                    presents.append(present)
+                rows = []
+            continue
+        if line.endswith(":"):
+            mode = 'present'
+            continue
+        if mode == 'present':
+            rows.append(line)
+            continue
+        parts = line.split(': ')
+        if len(parts) < 2:
+            continue
+        wh = [int(x) for x in parts.pop(0).split('x')]
+        quantities = [int(x) for x in parts.pop().split(' ')]
+        containers.append(((wh[0], wh[1]), quantities))
+    return presents, containers
 
 
 if __name__ == '__main__':
--- src/day_12/test_day_12.py
+++ src/day_12/test_day_12.py
@@ -1,11 +1,43 @@
 from .day_12 import solve_1, solve_2
 
-test_input = """
+test_input = """0:
+###
+##.
+##.
+
+1:
+###
+##.
+.##
+
+2:
+.##
+###
+##.
+
+3:
+##.
+###
+##.
+
+4:
+###
+#..
+###
+
+5:
+###
+.#.
+###
+
+4x4: 0 0 0 0 2 0
+12x5: 1 0 1 0 2 2
+12x5: 1 0 1 0 3 2
 """
 
 
 def test_solve_1():
-    assert solve_1(test_input, True) == 0
+    assert solve_1(test_input, True) == 2
 
 
 def test_solve_2():
